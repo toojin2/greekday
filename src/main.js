b5573488bf7160ccd8c9ac412c83783614e5f3ac
wlsdl1025 <147586183+wlsdl1025@users.noreply.github.com>
2024-02-14 09:47:06 +0900

    html

---
+++ src/main.js
@@ -0,0 +1,20 @@
+//https://wsss.tistory.com/479
+//버튼 호버시 효과
+$(function () {
+  $(".brand_btn")
+    .on("mouseenter", function (e) {
+      var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+      $(this).find(".brand_btn_pointer").css({ top: relY, left: relX });
+    })
+    .on("mouseout", function (e) {
+      var parentOffset = $(this).offset(),
+        relX = e.pageX - parentOffset.left,
+        relY = e.pageY - parentOffset.top;
+      $(this).find(".brand_btn_pointer").css({ top: relY, left: relX });
+    });
+});
+
+const trigger = new scrollTrigger.default();
+trigger.add("[data-trigger]");
